04dcaff9a36944cb16d02b97739f7dff01f067ce
wangcen17 <wangcen17@163.com>
2020-06-27 15:29:17 +0800

    Leetcode-344：reverses String
    Signed-off-by: wangcen17 <wangcen17@163.com>

--- reversesString.cpp
+++ reversesString.cpp
@@ -6,25 +6,38 @@ class Solution {
 public:
     void reverseString(vector<char>& s) {
         int len = s.size();
-        for(int i = 0; i < len/2; i++)
-        {
-            
-        }
+		printVector(s);
+		cout<< "len is" << len << endl;
+		int left = 0, right = len - 1;
+		while(left < right)
+		{
+			swap(s[left++], s[right--]);
+		}
+		cout << "after" << endl;
+		printVector(s);
     }
 	void printVector(vector<char> s){
 		for(auto i : s)
 		{
-			cout << i << " "; 
+			cout << i << " ";
 		}
 		cout << "" << endl;
 	}
+	void printVector(vector<string> s){
+	for(auto i : s)
+	{
+		cout << i << " "; 
+	}
+	cout << "" << endl;
+}
 };
 
 
 int main() {
 	Solution s;
-	vector<char> s1 = {"h", "e", "l", "l", "0"};
-	s.printVector(s1);
+	vector<char> s1 = {'h', 'e', 'l', 'l', 'o',};
+	s.reverseString(s1);
+	//s.printVector(s1);
     cout << "Hello W3Cschool!";
     return 0;
 }
